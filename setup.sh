00dce69990aa9fdc7a2cfc139849139a4f5a2e12
Justin Hunt <poodllsupport@gmail.com>
2016-09-25 13:45:18 +0000

    early changes

--- setup.sh
+++ setup.sh
@@ -10,4 +10,20 @@ mkdir moodledata
 chmod 777 moodledata
 
 #make moodle program directory
+cd workspace
+rm -f *
 git clone git@github.com:moodle/moodle.git workspace
+git branch --track MOODLE_31_STABLE origin/MOODLE_31_STABLE
+git checkout MOODLE_31_STABLE
+
+#make moodle config 
+cp /home/ubuntu/cloud9/config.php config.php
+sed -i 's/@@USERNAME@@/$1/g' config.php
+sed -i 's/@@WORKSPACE@@/$2/g' config.php
+
+#start database
+mysql-ctl start
+
+#perform installation
+php admin/cli/install.php
+
